129b5c06d546d8240488dbee95760ecb38b44cb0
noemie-papin <102947593+n0pap@users.noreply.github.com>
2023-11-22 20:53:00 +0100

    Lecon1_YT_React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
+function App() {
+  //state (état, données)
+  const [fruits, setFruits] = useState([
+    { id: 1, nom: "Abricot" },
+    { id: 2, nom: "Banane" },
+    { id: 3, nom: "Cerise" },
+  ]);
+  //const voiture = <li>Tesla</li>;
+  //const voitures = [<li>Fiat</li>, <li>Audi</li>, <li>GMG</li>];
+
+  const [nouveauFruits, setNouveauFruits] = useState("");
+
+  //comportements
+  const handleButton = (id) => {
+    //copie du state
+    const fruitCopy = fruits.slice();
+    //const fruitCopy=[...fruits]; fait la meme chose
+    const fruitCopyUpdated = fruitCopy.filter((fruit) => fruit.id !== id);
+    //manipulation du state
+
+    //modifier mon state avec le setter
+    setFruits(fruitCopyUpdated);
+  };
+
+  const handleChange = (event) => {
+    const valueAfterChange = event.target.value;
+    console.log(event.target.value);
+    setNouveauFruits(valueAfterChange);
+  };
+
+  //const inputRef = useRef();
+  const handleSubmit = (event) => {
+    event.preventDefault(); //ne refresh pas la page
+    //copie du state
+    const NouveauFruitCopy = [...fruits];
+    //manipulation du state
+    const id= new Date().getTime()
+    const nom =nouveauFruits
+    NouveauFruitCopy.push({id:id,nom:nom})
+
+    //modifier mon state avec le setter
+    setFruits(NouveauFruitCopy);
+    setNouveauFruits("");
+  };
+
+  //render
+  return (
+    <div>
+      <h1>Liste de fruits</h1>
+      <ul>
+        {fruits.map((fruit) => {
+          return (
+            <li key={fruit.id}>
+              {fruit.nom}
+              <button onClick={() => handleButton(fruit.id)}>X</button>
+            </li>
+          );
+        })}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={nouveauFruits}
+          type="text"
+          placeholder="Ajouter un fruit"
+          onChange={handleChange}
+        />
+        <button>Ajouter</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
+
+/*function App() {
+  //state (état, données)
+  const [fruits, setFruits] = useState([
+    { id: 1, nom: "Abricot" },
+    { id: 2, nom: "Banane" },
+    { id: 3, nom: "Cerise" },
+  ]);
+  const voiture = <li>Tesla</li>;
+  const voitures = [<li>Fiat</li>, <li>Audi</li>, <li>GMG</li>];
+  //comportements
+  const handleButton = (id) => {
+    //copie du state
+    const fruitCopy = fruits.slice();
+    //const fruitCopy=[...fruits]; fait la meme chose
+    const fruitCopyUpdated = fruitCopy.filter((fruit) => fruit.id !== id);
+    //manipulation du state
+
+    //modifier mon state avec le setter
+    setFruits(fruitCopyUpdated);
+  };
+  //render
+  return (
+    <div>
+      <h1>Liste de fruits</h1>
+      <ul>
+        {fruits.map((fruit) => {
+          return (
+            <li key={fruit.id}>
+              {fruit.nom}
+              <button onClick={() => handleButton(fruit.id)}>X</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}*/
+
+/*function App() {
+  //state (état, données)
+  const [compteur, setCompteur] = useState(1);
+  //comportements
+  const handleClick = () => {
+    setCompteur(compteur + 1);
+  };
+  //render
+  return (
+    <div>
+      <h1>{compteur}</h1>
+      <button onClick={handleClick}>Incrémentation</button>
+    </div>
+  );
+}*/
+
+/*
+import logo from "./logo.svg";
+import "./App.css";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Genre la
         </p>
         <a
           className="App-link"
@@ -20,6 +150,4 @@ function App() {
       </header>
     </div>
   );
-}
-
-export default App;
+}*/
