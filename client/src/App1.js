6937019f519302f8d9caf0c05702b698e80f0c1b
noemie-papin <102947593+n0pap@users.noreply.github.com>
2024-02-10 15:04:59 +0100

    DeplacementFichierOK

--- client/src/App1.js
+++ client/src/App1.js
@@ -1,31 +1,4 @@
-// App.js
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Accueil from './Accueil';
-import CreateOrJoin from './CreateOrJoin';
-import CreationParameterGame from './CreationParameterGame';
-import Room from './Room';
-
-const Stack = createStackNavigator();
-
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Accueil">
-        <Stack.Screen name="Accueil" component={Accueil} />
-        <Stack.Screen name="CreateOrJoin" component={CreateOrJoin} />
-        <Stack.Screen name="CreationParameterGame" component={CreationParameterGame} />
-        <Stack.Screen name="Room" component={Room} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
-export default App;
-
-
-/*import { useState } from "react";
+import { useState } from "react";
 
 function App() {
   //state (état, données)
@@ -100,6 +73,159 @@ function App() {
 }
 
 export default App;
+
+
+
+/*
+
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+      fetch("http://localhost:8080/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+      this.callAPI();
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p> ,
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+/*
+import React, { Component } from "react";
+
+class App extends Component {
+  //state (état, données)
+  state = {
+    post: {},
+  };
+
+  //comportements
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        this.setState({ post: result });
+        console.log(result);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Dernier message :</h1>
+        {this.state.post.title}
+      </div>
+    );
+  }
+}
+
+export default App;
+/*
+function App() {
+  //state (état, données)
+  const [fruits, setFruits] = useState([
+    { id: 1, nom: "Abricot" },
+    { id: 2, nom: "Banane" },
+    { id: 3, nom: "Cerise" },
+  ]);
+  //const voiture = <li>Tesla</li>;
+  //const voitures = [<li>Fiat</li>, <li>Audi</li>, <li>GMG</li>];
+
+  const [nouveauFruits, setNouveauFruits] = useState("");
+
+  //comportements
+  const handleButton = (id) => {
+    //copie du state
+    const fruitCopy = fruits.slice();
+    //const fruitCopy=[...fruits]; fait la meme chose
+    const fruitCopyUpdated = fruitCopy.filter((fruit) => fruit.id !== id);
+    //manipulation du state
+
+    //modifier mon state avec le setter
+    setFruits(fruitCopyUpdated);
+  };
+
+  const handleChange = (event) => {
+    const valueAfterChange = event.target.value;
+    console.log(event.target.value);
+    setNouveauFruits(valueAfterChange);
+  };
+
+  //const inputRef = useRef();
+  const handleSubmit = (event) => {
+    event.preventDefault(); //ne refresh pas la page
+    //copie du state
+    const NouveauFruitCopy = [...fruits];
+    //manipulation du state
+    const id= new Date().getTime()
+    const nom =nouveauFruits
+    NouveauFruitCopy.push({id:id,nom:nom})
+
+    //modifier mon state avec le setter
+    setFruits(NouveauFruitCopy);
+    setNouveauFruits("");
+  };
+
+  //render
+  return (
+    <div>
+      <h1>Liste de fruits</h1>
+      <ul>
+        {fruits.map((fruit) => {
+          return (
+            <li key={fruit.id}>
+              {fruit.nom}
+              <button onClick={() => handleButton(fruit.id)}>X</button>
+            </li>
+          );
+        })}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={nouveauFruits}
+          type="text"
+          placeholder="Ajouter un fruit"
+          onChange={handleChange}
+        />
+        <button>Ajouter</button>
+      </form>
+    </div>
+  );
+}
+*/
 
 /*function App() {
   //state (état, données)
